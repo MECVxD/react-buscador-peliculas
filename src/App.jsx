1aab9bd7d9a502fa410681a6b7354c1b9fc2d25f
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2024-08-27 13:10:00 -0600

    Added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
-import './App.css'
-import { useRef } from 'react'
+import { useSearch } from './hooks/useSearch'
 import { useState } from 'react'
+import './App.css'
 
 function App() {
-  const {movies} = useMovies()
-  const [query, setQuery] = useState('')
-
+  const [sort, setSort] = useState(false)
+  const {search, updateSearch, error} = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search, sort })
+  
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(query);
+    getMovies()
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
   }
 
   const handleChange = (event) => {
-    setQuery(event.target.value)
+    updateSearch(event.target.value)
   }
 
   return (
@@ -22,12 +27,26 @@ function App() {
       <header>
         <h1>Buscador de peliculas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input onChange={handleChange} value={query} name='query' type="text" placeholder='Avenger, Star Wars, The Matrix...' />
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} 
+            onChange={handleChange}
+            value={search}
+            name='query'
+            type="text"
+            placeholder='Avengers, Star Wars, The Matrix...' 
+          />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
       <main>
-        <Movies movies={movies}/>
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
       </main>
     </div>
   )
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,15 +1,33 @@
-import responseMovies from '../mocks/with-results.json'
-import withoutResults from '../mocks/no-results.json'
+import { searchMovies } from '../services/movies'
+import { useRef, useState, useMemo } from 'react'
 
-export function useMovies() {
-  const movies = responseMovies.Search
+export function useMovies({search, sort}) {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const previousSearch = useRef(search)
 
-  const mappedMovies = movies?.map(movie => ({
-    id: movie.imdbID,
-    title: movie.title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
+  
+  const getMovies = async () => {
+    if(search === previousSearch.current) return
+    try {
+      setLoading(true)
+      setError(null)
+      previousSearch.current = search
+      const newMovies = await searchMovies({search})
+      setMovies(newMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      setLoading(false)
+    }
+  }
 
-  return {movies: mappedMovies}
+  const sortedMovies = useMemo(() => {
+    return sort 
+    ? [...movies].sort((a, b) => a.title.localeCompare(b.title)) 
+    : movies
+  }, [sort, movies])
+
+  return {movies: sortedMovies, getMovies, loading}
 }
